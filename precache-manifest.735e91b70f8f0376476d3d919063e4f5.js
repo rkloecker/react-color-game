03031f2dc2cdbb9f24c1637c5f4fd1eb25cc8dff
R K <blah@eample.com>
2018-12-03 12:37:40 +0100

    Updates

---
+++ precache-manifest.735e91b70f8f0376476d3d919063e4f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87573d78553a8e0a6fad",
+    "url": "/react-color-game/static/js/runtime~main.87573d78.js"
+  },
+  {
+    "revision": "d5776b71df1bcacee589",
+    "url": "/react-color-game/static/js/main.d5776b71.chunk.js"
+  },
+  {
+    "revision": "fe65e5159218a8a08e5e",
+    "url": "/react-color-game/static/js/1.fe65e515.chunk.js"
+  },
+  {
+    "revision": "d5776b71df1bcacee589",
+    "url": "/react-color-game/static/css/main.9920b84d.chunk.css"
+  },
+  {
+    "revision": "e86e7dfa04cc8442ddf7a45ed8e6eb9d",
+    "url": "/react-color-game/index.html"
+  }
+];
